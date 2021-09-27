14e97c244fc493d3d66fbcbdccdea6952ceecb8a
m-chiu <m-chiu@XW-000341noMacBook-Pro.local>
2021-09-27 17:58:58 +0900

    add: a simple json -> probuf example

--- transition_server/main.go
+++ transition_server/main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"context"
+	"io/ioutil"
 	"log"
 	"net"
 	pb "probuf_reflect_test/transition"
 
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 const port = ":50052"
+const pathJsonSimple = "./res/simple.json"
 
 type serverImpl struct {
 	pb.UnimplementedTransitionServer
 }
 
 func (s *serverImpl) Simple(ctx context.Context, param *pb.Param) (*pb.Res, error) {
-	return &pb.Res{Value: 52}, nil
+	json, err := ioutil.ReadFile(pathJsonSimple)
+	if err != nil {
+		log.Fatalf("error occurs when parsing json file: %v", err)
+	}
+	res := pb.Res{}
+	if err = protojson.Unmarshal(json, &res); err != nil {
+		log.Fatalf("error occurs when unmarshaling json data: %v", err)
+	}
+	return &res, nil
 }
 
 func main() {
