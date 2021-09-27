0b110b20221dab30ffb29af686cc545978e53cf6
m-chiu <m-chiu@XW-000341noMacBook-Pro.local>
2021-09-27 20:06:17 +0900

    rename param for readibility

--- transition_server/main.go
+++ transition_server/main.go
@@ -14,17 +14,17 @@ import (
 const port = ":50052"
 const pathJsonSimple = "./res/simple.json"
 
-type serverImpl struct {
+type transitionImpl struct {
 	pb.UnimplementedTransitionServer
 }
 
-func (s *serverImpl) Simple(ctx context.Context, param *pb.Param) (*pb.Res, error) {
-	json, err := ioutil.ReadFile(pathJsonSimple)
+func (s *transitionImpl) Simple(ctx context.Context, param *pb.Param) (*pb.Res, error) {
+	jsonb, err := ioutil.ReadFile(pathJsonSimple)
 	if err != nil {
 		log.Fatalf("error occurs when parsing json file: %v", err)
 	}
 	res := pb.Res{}
-	if err = protojson.Unmarshal(json, &res); err != nil {
+	if err = protojson.Unmarshal(jsonb, &res); err != nil {
 		log.Fatalf("error occurs when unmarshaling json data: %v", err)
 	}
 	return &res, nil
@@ -36,7 +36,7 @@ func main() {
 		log.Fatalf("error occurs: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	pb.RegisterTransitionServer(grpcServer, &serverImpl{})
+	pb.RegisterTransitionServer(grpcServer, &transitionImpl{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
